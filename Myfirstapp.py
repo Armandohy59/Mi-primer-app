4f29491763cd59578b4e611e725bfa8a21df041d
Armandohy59 <102831631+Armandohy59@users.noreply.github.com>
2022-04-01 14:33:43 -0700

    Oculte todo

--- Myfirstapp.py
+++ Myfirstapp.py
@@ -1,11 +1,12 @@
 import streamlit as st
 st.title("Mi primer App")
-st.button("Dale click")
-st.button("Dale click x2")
+s#t.button("Dale click")
+#st.button("Dale click x2")
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
+#Las siguientes lineas de código eran para visualizar
+#df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
 
-st.write(df)
+#st.write(df)
                  
-st.map(df)
+#st.map(df)
