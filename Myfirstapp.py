98dc983a7a4f5f6a34aeba16984b9cb21f33a7fe
Armandohy59 <102831631+Armandohy59@users.noreply.github.com>
2022-04-01 14:25:20 -0700

    Activé boton

--- Myfirstapp.py
+++ Myfirstapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("Mi primer App")
-#st.button("Dale click")
-#st.button("Dale click x2")
+st.button("Dale click")
+st.button("Dale click x2")
 import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
